59cb902195d4def0b233fc869462d9124bc51e61
KOMATSU Issei <issei@mizuhataya.com>
2013-04-06 00:10:36 +0900

    地図をImageViewからWebViewに変更

--- src/client/titanium/app/controllers/index.js
+++ src/client/titanium/app/controllers/index.js
@@ -1,3 +1,5 @@
+var apiMapper = new ApiMapper();
+
 // navbar の設定
 var button_badges = Ti.UI.createButton({title:'Badges'});
 var button_start_quiz = Ti.UI.createButton({title:'Quiz'});
@@ -14,6 +16,30 @@ button_start_quiz.addEventListener('click', function(){
 
 $.win.leftNavButton = button_badges;
 $.win.rightNavButton = button_start_quiz;
+
+// 地図情報取得
+apiMapper.establish(
+    {
+        user_id: Alloy.Globals.user.user_id 
+    },
+    function(e){
+        var response = JSON.parse(this.responseText);
+
+        var param_styles = new Array();
+        for( var i=0; i<response.length; i++){
+            param_styles.push("bm.p." + response[i].Establish.city_id + ":cc0000");
+        }
+
+        // 地図画像取得
+        var image_url = "http://map.olp.yahooapis.jp/OpenLocalPlatform/V1/static?appid=buxd.Puxg662idT4Kd8LGdoNWBKywhPDbp4dfPupYfrbu.0ICiES_bkM9iImHA--&lat=38.2&lon=140.6&z=9&height=1600&width=600&mode=blankmap&style=" + param_styles.join("|");
+        Ti.API.debug(image_url);
+        $.establish_map.image = image_url;
+        $.establish_map.url = image_url;
+    },
+    function(e){
+        alert('制覇情報の取得に失敗しました');
+    }
+);
 
 // View 表示
 $.index.open();
